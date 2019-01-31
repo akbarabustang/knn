840e9226475badefd5eff14212dd588fd29d1897
akbarabustang <akbar_abustang@ymail.com>
2019-01-31 10:59:23 +0700

    Update Convertion.php

--- application/controllers/Convertion.php
+++ application/controllers/Convertion.php
@@ -13,6 +13,90 @@ class Convertion extends CI_Controller {
         $this->load->view('front/home');
     }
 
+    public function test1()
+    {
+
+
+        $toHSV = $this->RGBtoHSV(174,150,51);
+
+        echo "<pre>";
+        print_r($toHSV);
+        echo "</pre>";
+
+
+    }
+
+
+    //tasks to do
+    //mean
+    //knn
+
+
+
+
+    function RGBtoHSV($rgb = array())
+    {
+
+       $HSV = array();
+
+       // print_r($rgb);exit;
+
+        $R = '';
+        $G = '';
+        $B = '';
+
+        foreach($rgb as $key ){
+            foreach ($key as $key2 => $value) {
+                $R = ($value['R'] / 255);
+                $G = ($value['G'] / 255);
+                $B = ($value['B'] / 255);
+
+
+                $maxRGB = max($R, $G, $B);
+                $minRGB = min($R, $G, $B);
+                $chroma = $maxRGB - $minRGB;
+
+
+                $computedV = 100 * $maxRGB;
+
+                if ($chroma == 0)
+                    return array(0, 0, $computedV);
+
+
+                $computedS = 100 * ($chroma / $maxRGB);
+
+
+                if ($R == $minRGB)
+                    $h = 3 - (($G - $B) / $chroma);
+                elseif ($B == $minRGB)
+                    $h = 1 - (($R - $G) / $chroma);
+                else
+                    $h = 5 - (($B - $R) / $chroma);
+
+                $computedH_ =  60 * $h;;
+                $computedH = $computedH_ * 255 / 360;
+
+               $HSV[]['H'] = $computedH;
+               $HSV[]['S'] = $computedS;
+               $HSV[]['V'] = $computedV;
+
+                   // $HSV[]['H'] = 1;
+                   // $HSV[]['S'] = 1;
+                   // $HSV[]['V'] = 1;
+
+
+            }
+        }
+
+        echo "<pre>";
+        print_r($HSV);
+        echo "</pre>";
+        exit;
+
+        return $HSV;
+    }
+
+
 	public function getImageRGB($source)
 	{
 
@@ -26,6 +110,8 @@ class Convertion extends CI_Controller {
 
         for ($y = 0; $y < $height; $y++) {
             $y_array = array() ;
+
+
 
             for ($x = 0; $x < $width; $x++) {
                 $rgb = imagecolorat($image, $x, $y);
@@ -43,7 +129,8 @@ class Convertion extends CI_Controller {
 
         }
 
-        $toHSV = $this->rgbTohsv($colors);
+        $toHSV = $this->rgbTohsv1($colors);
+
 
         $name = 'DETEKSI-'.uniqid(4);
         $insert = $this->db->query("INSERT INTO data_deteksi (name, image, rgb, hsv) VALUES ('".$name."', '".$source."' ,'".serialize($colors)."' , '".serialize($toHSV)."') ");
@@ -56,64 +143,7 @@ class Convertion extends CI_Controller {
 
     }
 
-    public function rgbTohsv($rgb = array())
-    {
 
-       $HSL = array();
-        // echo json_encode(array('data'=>$rgb));exit;
-
-
-        $var_R = '';
-       $var_G = '';
-       $var_B = '';
-
-       foreach ($rgb as $key ) {
-            foreach ($key as $key2 => $value) {
-                 // print_r($value);exit;
-            // print_r($rgb);exit;
-
-            $var_R = $value['R'];
-            $var_G = $value['G'];
-            $var_B = $value['B'];
-
-
-
-            $var_Min = min($var_R, $var_G, $var_B);
-           $var_Max = max($var_R, $var_G, $var_B);
-           $del_Max = $var_Max - $var_Min;
-           $V = $var_Max;
-
-           if ($del_Max == 0)
-           {
-              $H = 0;
-              $S = 0;
-           }
-           else
-           {
-              $S = $del_Max / $var_Max;
-              $del_R = ( ( ( $var_Max - $var_R ) / 6 ) + ( $del_Max / 2 ) ) / $del_Max;
-              $del_G = ( ( ( $var_Max - $var_G ) / 6 ) + ( $del_Max / 2 ) ) / $del_Max;
-              $del_B = ( ( ( $var_Max - $var_B ) / 6 ) + ( $del_Max / 2 ) ) / $del_Max;
-              if      ($var_R == $var_Max) $H = $del_B - $del_G;
-              else if ($var_G == $var_Max) $H = ( 1 / 3 ) + $del_R - $del_B;
-              else if ($var_B == $var_Max) $H = ( 2 / 3 ) + $del_G - $del_R;
-              if ($H<0) $H++;
-              if ($H>1) $H--;
-
-           }
-
-           $HSL['H'] = $H;
-           $HSL['S'] = $S;
-           $HSL['V'] = $V;
-        }
-
-
-       }
-
-
-
-       return $HSL;
-    }
 
     public function imageResize()
     {
@@ -165,9 +195,71 @@ class Convertion extends CI_Controller {
     }
 
 
+
+    //gak dipake
     public function getImageConvertion()
     {
         $data = $this->db->query(" SELECT * FROM data_deteksi ORDER BY id DESC LIMIT 0,1 ")->result();
         echo json_encode(array('data'=>$data));
+    }
+
+
+
+    public function rgbTohsv1($rgb = array())
+    {
+
+       $HSL = array();
+        // echo json_encode(array('data'=>$rgb));exit;
+
+
+       $var_R = '';
+       $var_G = '';
+       $var_B = '';
+
+       foreach ($rgb as $key ) {
+            foreach ($key as $key2 => $value) {
+                 // print_r($value);exit;
+            // print_r($rgb);exit;
+
+            $var_R = $value['R'];
+            $var_G = $value['G'];
+            $var_B = $value['B'];
+
+
+
+            $var_Min = min($var_R, $var_G, $var_B);
+           $var_Max = max($var_R, $var_G, $var_B);
+           $del_Max = $var_Max - $var_Min;
+           $V = $var_Max;
+
+           if ($del_Max == 0)
+           {
+              $H = 0;
+              $S = 0;
+           }
+           else
+           {
+              $S = $del_Max / $var_Max;
+              $del_R = ( ( ( $var_Max - $var_R ) / 6 ) + ( $del_Max / 2 ) ) / $del_Max;
+              $del_G = ( ( ( $var_Max - $var_G ) / 6 ) + ( $del_Max / 2 ) ) / $del_Max;
+              $del_B = ( ( ( $var_Max - $var_B ) / 6 ) + ( $del_Max / 2 ) ) / $del_Max;
+              if      ($var_R == $var_Max) $H = $del_B - $del_G;
+              else if ($var_G == $var_Max) $H = ( 1 / 3 ) + $del_R - $del_B;
+              else if ($var_B == $var_Max) $H = ( 2 / 3 ) + $del_G - $del_R;
+              if ($H<0) $H++;
+              if ($H>1) $H--;
+
+           }
+
+           $HSL[]['H'] = $H;
+           $HSL[]['S'] = $S;
+           $HSL[]['V'] = $V;
+        }
+
+
+       }
+
+
+       return $HSL;
     }
 }
